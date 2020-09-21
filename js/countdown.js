369781e00731f9ff68f0a1efefaf90e1825970e6
Gabriel Rodriguez <gaberodriguez43@gmail.com>
2020-09-21 00:15:24 -0500

    countdown

---
+++ js/countdown.js
@@ -0,0 +1,19 @@
+var countDownDate = new Date("September 25, 2020 15:37:00").getTime();
+
+var oneTicInterval = setInterval(function() {
+  var currentDate = new Date().getTime();
+  var dateDiff = countDownDate - currentDate;
+  var days = Math.floor(dateDiff/(1000*60*60*24));
+  var hours = Math.floor((dateDiff%(1000*60*60*24))/(1000*60*60));
+  var minutes = Math.floor((dateDiff%(1000*60*60))/(1000*60));
+  var seconds = Math.floor((dateDiff%(1000*60))/(1000));
+  document.getElementById("countDownClock").innerHTML = days + " Days, " + hours + " Hours, "
+	
+  + minutes + " Minutes, and " + seconds + " seconds." ;
+ 
+  if (dateDiff < 0) {
+    clearInterval(oneTicInterval);
+    document.getElementById("countDownClock").innerHTML = "What are you doing!? The game's already started!";
+	document.getElementById("frontPageSignUp").innerHTML = "";
+  }
+}, 1000);
